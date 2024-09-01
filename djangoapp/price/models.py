770a2d1a7e5e9036f0de78120101a3c36dc27e5f
tiago giglia manoel <tiago.g.manoel@gmail.com.br>
2024-09-01 10:40:29 -0300

    conection from DB add

--- djangoapp/price/models.py
+++ djangoapp/price/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class pi_db (models.Model):
+    date = models.TextField()
+    value = models.FloatField()
+    currency = models.TextField(max_length=3)
+    
